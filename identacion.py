bd1bc7bf92cdd126528c78d36c4c413a1589e9ff
Statick <dsaavedra88@gmail.com>
2022-03-08 21:16:36 -0500

    Identación, Variables y Ejercicio 1

---
+++ identacion.py
@@ -0,0 +1,13 @@
+"""
+Ejemplos de Identacion,
+en este script voy a crear varios ejemplos
+de identacion
+"""
+
+def nombre(): #Esto es una funcion llamada nombre
+    nombre = "Juan" # Esto es una variable llamada nombre que almacena el valor Juan
+    print(nombre) #Esto es una funcion print que llama al valor almacenado en la variable nombre
+
+nombre()
+# Aqui termina la ejecucion del codigo
+
