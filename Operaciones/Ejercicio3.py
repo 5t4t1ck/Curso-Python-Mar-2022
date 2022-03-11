544016993a7bf41e8c805e94095552e899c107a4
Diego Saavedra <dsaavedra88@gmail.com>
2022-03-10 20:01:22 -0500

    Resolucion del ejercicio 3

--- Operaciones/Ejercicio3.py
+++ Operaciones/Ejercicio3.py
@@ -1,3 +1,25 @@
-"""Crear una lista con los nombres de 5 estudiantes
+"""
+Crear una lista con los nombres de 5 estudiantes
 Crear una lista2 con 5 notas
-Crear una lista3 que permita obsevar los nombres y las notas"""
+Crear una lista3 que permita obsevar los nombres y las notas
+"""
+
+"""
+Paso 0: Inicio
+Paso 1: crear la lista y poner entre corchetes separados por comas 5 nombres
+Paso 2: crear una lista2 y poner 5 notas separadas por comas en 2 corchetes
+Paso 3: crear lista3 he idear la forma de cotejar los datos de la lista y lista2
+Paso 4: presentar los datos por terminal
+Paso 5: Fin
+"""
+
+lista = ["Juan", "Maria", "Pedro", "Luis", "Juana"]
+lista2 = [9.3, 8.7, 10, 10, 6.5]
+
+print("Estudiante 1: ")
+lista3 = [lista[0], lista2[0]]
+print(lista3)
+
+print("Estudiante 2: ")
+lista4 = [lista[1], lista2[1]]
+print(lista4)
